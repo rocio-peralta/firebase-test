e85a873bf86e6576e6b8a9131c5064075f029fcb
Rocio-Peralta <1rocioperalta@gmail.com>
2024-04-09 17:48:01 +1200

    auth done

--- prueba-firebase/src/components/Auth.jsx
+++ prueba-firebase/src/components/Auth.jsx
@@ -1,10 +1,52 @@
-const Auth  = () => {
-    return (
-        <div>
-           <input placeholder="Email" type="email"/>
-           <input placeholder="Password" type="password"/>
-           <button>Sign In</button>
-        </div>
-    )
+import { useState } from 'react'
+import { auth, googleProvider  } from '../config/firebase'
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+
+const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  console.log(auth?.currentUser?.photoURL)
+
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const logout = async () => {
+    try {
+      await signOut(auth)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <div>
+      <input
+        placeholder="Email"
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signIn}>Sign In</button>
+
+      <button onClick={signInWithGoogle}>Sign in wiht Gmail</button>
+      <button onClick={logout}>Sign Out</button>
+    </div>
+  )
 }
 export default Auth
