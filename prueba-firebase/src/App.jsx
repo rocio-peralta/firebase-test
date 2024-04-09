d6114710362ff84fd17643b0ef35a91d24399740
Rocio-Peralta <1rocioperalta@gmail.com>
2024-04-09 19:31:08 +1200

    add movie done

--- prueba-firebase/src/App.jsx
+++ prueba-firebase/src/App.jsx
@@ -1,14 +1,70 @@
 import Auth from './components/Auth'
+import { db } from './config/firebase'
+import { getDocs, collection, addDoc, deleteDoc } from 'firebase/firestore'
 
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [movieList, setMovieList] = useState([])
 
+  // New Movie States
 
+  const [newMovieTitle, setNewMovieTitle] = useState('')
+  const [newReleaseDate, setNewReleaseDate] = useState(0)
+  const[isNewMovieOscar, setIsNewMovieOscar] = useState(false)
+
+  const moviesCollectionRef = collection(db, 'movies')
+
+  const getmovieList = async () => {
+  try {
+     const data = await getDocs(moviesCollectionRef)
+     const filterDAta = data.docs.map(doc => ({...doc.data(), id: doc.id}))
+      setMovieList(filterDAta)
+  } catch (error) {
+    console.log(error)
+  }
+  }
+
+  const deleteMovie = async (id) => {
+    await deleteDoc(doc(moviesCollectionRef, id))
+  }
+  useEffect(() => {
+    getmovieList()
+  }, [])
+
+   const onSubmitMovie = async () => {
+     try {
+       await addDoc(moviesCollectionRef, {title: newMovieTitle, releaseDate: newReleaseDate, oscar: isNewMovieOscar})
+      getmovieList()
+      } catch (error) {
+      console.log(error)
+     }
+   }
   return (
     <>
-     <Auth/>
-       
+      <Auth/>
+
+     <div>
+      <input placeholder='Movie' onChange={(e)=> setNewMovieTitle(e.target.value)}/>
+      <input placeholder='Release date..' type='number' onChange={(e)=> setNewReleaseDate(Number(e.target.value))}/>
+      <input type='checkbox' checked={isNewMovieOscar} onChange={(e)=>setIsNewMovieOscar(e.target.checked)}/>
+      <label>Recieved an Oscar</label>
+      <button onClick={onSubmitMovie}>Add Movie</button>
+     </div>
+
+
+
+
+      <div>
+        {movieList.map((movie, index) => (
+          <div key={index}>
+            <h1 style={{color: movie.oscar ? "green" : "red"}}>{movie.title}</h1>
+            <p>Date:{movie.releaseDate}</p>
+        
+          </div>
+        ))}
+      </div>
     </>
   )
 }
